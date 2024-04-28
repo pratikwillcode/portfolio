1e6a624c8a3272a631f57d6ab43dbe399673d4ac
Pratik Awari <64844000+pratikwillcode@users.noreply.github.com>
2024-04-28 19:01:30 +0530

    Update blockContent.ts

--- schemas/blockContent.ts
+++ schemas/blockContent.ts
@@ -9,38 +9,39 @@ export default defineType({
     defineArrayMember({
       title: "Block",
       type: "block",
-      styles: [
-        { title: "Normal", value: "normal" },
-        { title: "H2", value: "h2" },
-        { title: "H3", value: "h3" },
-        { title: "H4", value: "h4" },
-        { title: "Quote", value: "blockquote" },
-      ],
-      marks: {
-        decorators: [
-          { title: "Strong", value: "strong" },
-          { title: "Emphasis", value: "em" },
-          { title: "Code", value: "code" },
-        ],
-      },
+      // styles: [
+      //   { title: "Normal", value: "normal" },
+      //   { title: "H2", value: "h2" },
+      //   { title: "H3", value: "h3" },
+      //   { title: "H4", value: "h4" },
+      //   { title: "Quote", value: "blockquote" },
+      // ],
+      // marks: {
+      //   decorators: [
+      //     { title: "Strong", value: "strong" },
+      //     { title: "Emphasis", value: "em" },
+      //     { title: "Code", value: "code" },
+      //   ],
+      // },
+      
     }),
     defineArrayMember({
       type: "image",
       options: { hotspot: true },
-      fields: [
-        {
-          name: "caption",
-          title: "Image caption",
-          type: "string",
-          description: "Text displayed below the image.",
-        },
-        {
-          name: "alt",
-          title: "Alt text",
-          type: "string",
-          description: "Important for SEO and accessiblity.",
-        },
-      ],
+      // fields: [
+      //   {
+      //     name: "caption",
+      //     title: "Image caption",
+      //     type: "string",
+      //     description: "Text displayed below the image.",
+      //   },
+      //   {
+      //     name: "alt",
+      //     title: "Alt text",
+      //     type: "string",
+      //     description: "Important for SEO and accessiblity.",
+      //   },
+      // ],
     }),
     defineArrayMember({
       type: "code",
