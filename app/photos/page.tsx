946a3f7362f3cc0613feb0a43cc3ec3d42ef5522
Pratik Awari <64844000+pratikwillcode@users.noreply.github.com>
2024-04-29 08:19:16 +0530

    Update page.tsx

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -19,15 +19,13 @@ const images = [
 ];
 
 export const metadata: Metadata = {
-  title: "Photos | Victor Eke",
-  metadataBase: new URL("https://victoreke.com/photos"),
-  description: "Explore photos taken by Victor Eke",
+  title: "Photos | Pratik Awari",
+  metadataBase: new URL("https://portfolio-pratik-awari.vercel.app/photos"),
+  description: "Explore photos taken by Pratik Awari",
   openGraph: {
-    title: "Photos | Victor Eke",
-    url: "https://victoreke.com/photos",
-    description: "Explore photos taken by Victor Eke",
-    images:
-      "https://res.cloudinary.com/victoreke/image/upload/v1692635149/victoreke/photos.png",
+    title: "Photos | Pratik Awari",
+    url: "https://portfolio-pratik-awari.vercel.app/photos",
+    description: "Explore photos taken by Pratik Awari",
   },
 };
 
